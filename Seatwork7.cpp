aa24db532fb34ad20da4b3c02c4ac1444d8c26c4
AintMikazuki <157221305+AintMikazuki@users.noreply.github.com>
2024-02-14 07:57:06 +0800

    Add files via upload

---
+++ Seatwork7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int num[5];
+    int z = 0;
+
+    cout<<"Input your number for the array"<<endl;
+
+
+    for (z =0; z < 5; z++)
+    {
+          cout<<"Enter a number "<<z<<": ";
+          cin>>num[z];
+
+    }
+
+    cout<<endl;
+    cout<<"The value You Input"<<endl;
+
+    for (z = 0; z < 5; z++)
+    {
+        cout<<"Your inputs "<<z<<": "<<num[z]<<endl;
+    }
+
+    return EXIT_SUCCESS;
+}
