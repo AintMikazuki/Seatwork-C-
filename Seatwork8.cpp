3429b26f6d9716fcab3fd9a543d17e9992d6bbcc
AintMikazuki <157221305+AintMikazuki@users.noreply.github.com>
2024-02-14 08:27:17 +0800

    Add files via upload

---
+++ Seatwork8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int num[6];
+    int z = 0, sum = 0;
+
+    cout<<"Number for the Sum "<<endl;
+
+    for(z = 0; z < 6; z++)
+    {
+        cout<<"Enter a Number "<<z<<": ";
+        cin>>num[z];
+    }
+
+    for (z = 0; z < 6; z++)
+    {
+        sum += num[z];
+    }
+
+    cout<<"The total sum is: "<<sum<<endl<<endl;
+}
